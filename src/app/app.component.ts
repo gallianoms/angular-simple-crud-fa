25d2b515fb58ff99935c8f34762f00e11989224e
vluespider <vluespider@gmail.com>
2023-02-10 18:34:35 -0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Employee} from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+
+  employees: Employee[] = [
+    {
+      id: 1, name: 'Salvador J', country: 'USA'
+    },
+    {
+      id: 2, name: 'Test', country: 'USA'
+    },
+    {
+      id: 3, name: 'Test 2', country: 'USA'
+    }
+  ];
+
+  // Crea instancia de la clase Employee para el doble enlace de datos
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employees.length + 1;
+      this.employees.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee) {
+    // console.log(employee);
+    this.selectedEmployee = employee;
+  }
+
+  deleteEmployee() {
+    // La funcion filter lo que hace es guardar en el arreglo todos los empleados que son distintos del que esta seleccionado en los input
+    if (confirm('Are you sure to delete it?')) {
+      this.employees = this.employees.filter(x => x !== this.selectedEmployee);
+    }
+    // Limpiar el formulario
+    this.selectedEmployee = new Employee();
+  }
+
 }
